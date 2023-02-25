e25c1f757fa162f0ee6fd68d24647c6274c8b7be
Carla <107934506+bafisofi@users.noreply.github.com>
2023-02-25 13:08:21 +0300

    Commit initial files

---
+++ data.js
@@ -0,0 +1,56 @@
+export const dataCats =[
+    {
+        emotionWord: "happy",
+        emotionImage: "HappyCat.png",
+        image: "happy.png",
+        gif: "gifHappy.gif",
+    },
+     {   
+        emotionWord: "grateful",
+        emotionImage: "GratefulCat.png",
+        image: "grateful.png",
+        gif: "gifGrateful.gif",
+    },
+    {
+        emotionWord: "calm",
+        emotionImage: "CalmCat.png",
+        image: "calm.png",
+        gif: "gifCalm.gif",
+    },
+    {
+        emotionWord: "sad",
+        emotionImage: "SadCat.png",
+        image: "sad.png",
+        gif: "gifSad.gif",
+    },
+      {
+        emotionWord: "angry",
+        emotionImage: "AngryCat.png",
+        image: "angry.png",
+        gif: "gifAngry.gif",
+    },
+      {
+        emotionWord: "anxious",
+        emotionImage: "AnxiousCat.png",
+        image: "anxious.png",
+        gif: "gifAnxious.gif",
+    },
+       {
+        emotionWord: "scared",
+        emotionImage: "ScaredCat.png",
+        image: "scared.png",
+        gif: "gifScared.gif",
+    },
+        {
+        emotionWord: "sleepy",
+        emotionImage: "SleepyCat.png",
+        image: "sleepy.png",
+        gif: "gifSleepy.gif",
+    },
+         {
+        emotionWord: "powerful",
+        emotionImage: "PowerfulCat.png",
+        image: "powerful.png",
+        gif: "gifPowerful.gif",
+    },
+]
---
+++ index.js
@@ -0,0 +1,117 @@
+import{dataCats} from `/data.js`
+
+const emotionBtns = document.getElementById("emotion-btns");
+const getImageBtn = document.getElementById("get-image-btn");
+const memeModal = document.getElementById("meme-modal")
+const memeModalInner = document.getElementById("meme-modal-inner");
+const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
+const gifsOnlyOption =document.getElementById('gifs-only-option')
+const main = document.getElementsByTagName("main");
+
+emotionBtns.addEventListener("click",highlightCheckedOption)
+
+memeModalCloseBtn.addEventListener('click', closeModal)
+
+window.addEventListener("click", windowOnClick);
+
+getImageBtn.addEventListener("click",renderCat)
+
+
+function highlightCheckedOption(e){
+    const btnArray = document.getElementsByClassName("btn-emotion");
+    for (let btn of btnArray){
+        btn.classList.remove('highlight')
+    }
+     if(e.target.id !=='emotion-btns'){
+        document.getElementById(e.target.id).classList.add('highlight')
+    }
+}
+
+
+function closeModal(){
+    memeModal.style.display = 'none'
+}
+
+
+
+function windowOnClick(event) {
+    if (event.target === main[0]) {
+        memeModal.style.display = 'none'
+    }
+}
+
+function renderCat(){
+    const catImg = getImgCats()
+    memeModalInner.innerHTML= `<img 
+        class="cat-img" 
+        src="./img/${catImg}"
+        alt="Cat making our days better"
+        >`
+    memeModal.style.display = 'flex';
+    memeModal.classList.toggle("show-modal");    
+}
+
+
+function getImgCats(){
+    const isGif = gifsOnlyOption.checked;
+    if(document.getElementsByClassName("highlight")[0]){
+        const emotionSelected = document.getElementsByClassName("highlight")[0].value
+        for (let cat of dataCats){
+            if(cat.emotionWord ===emotionSelected){
+                if(isGif){
+                    return cat.gif 
+                }else {
+                    return cat.image 
+                }
+            }
+        }
+    }
+}
+
+
+function getEmotions(cats){
+    const emotionArray =[];
+    for(let cat of cats){
+        emotionArray.push(cat.emotionWord)
+        
+    }
+    return emotionArray
+}
+
+function getEmotionsImgs(cats){
+    const emotionImgArray=[]
+    for( let cat of cats){
+        emotionImgArray.push(cat.emotionImage)
+    }
+    return emotionImgArray
+}
+
+
+function renderEmotionsBtn(cats){
+    let btnItems =` `
+    const emotions = getEmotions(cats)
+    const emotionsImgs = getEmotionsImgs(cats)
+
+  emotions.forEach((emotion,index)=>{
+      const emotionImg = emotionsImgs[index]
+      btnItems+=`
+      <button 
+       type="submit" 
+       class="btn-emotion"
+       value ="${emotion}""
+       id ="${emotion}">
+       <img src="img/${emotionImg}"
+       id ="${emotion}"
+       alt="A ${emotion} Cat">
+       ${emotion}
+      </button>`
+  })
+   emotionBtns.innerHTML = btnItems    
+}
+
+
+renderEmotionsBtn(dataCats)
+
+
+   
+
